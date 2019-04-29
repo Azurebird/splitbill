8b7c52f0f7ba9d448a52913b8f56d3f738018937
Azurebird <white.camilo@gmail.com>
2019-04-29 18:06:07 -0500

    Redux added to the project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 import App from "./App";
+import rootReducer from "./rootReducer";
 import * as serviceWorker from "./serviceWorker";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
