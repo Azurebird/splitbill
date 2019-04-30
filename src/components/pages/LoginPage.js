ae414e991e276f70423dc2ab9d75832feb25c147
Azurebird <white.camilo@gmail.com>
2019-04-30 06:24:22 -0500

    Added some reducers and web logic

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,12 @@
 import LoginForm from "../forms/LoginForm";
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { login } from "../../actions/login";
 
 class LoginPage extends Component {
-  submit = data => {
-    console.log(data);
-  };
+  submit = data =>
+    this.props.login(data).then(() => this.props.history.push("/"));
 
   render() {
     return (
@@ -16,4 +18,14 @@ class LoginPage extends Component {
   }
 }
 
-export default LoginPage;
+LoginForm.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired,
+  login: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { login }
+)(LoginPage);
